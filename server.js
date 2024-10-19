0478471fddafed387642b64475319ac718563163
biswajit deori <74303983+BiswajitDeori@users.noreply.github.com>
2024-10-20 00:44:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Allow requests from your frontend
+    origin: "https://peertopeer-ui.onrender.com", // Allow requests from your frontend
     methods: ["GET", "POST"],
     credentials: true,
   },
